594c40b7138274ff64481d3abe9f8810d31c92d6
chrisbarbour <bobjamin@live.co.uk>
2018-07-15 18:24:52 +0100

    initial commit

--- src/WebGlCanvas.kt
+++ src/WebGlCanvas.kt
@@ -1,25 +1,40 @@
 
+import kotlinx.html.js.onMouseDownFunction
+import kotlinx.html.js.onMouseMoveFunction
+import kotlinx.html.js.onMouseUpFunction
+import org.w3c.dom.events.Event
+import org.w3c.dom.events.MouseEvent
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
 import react.dom.*
 
-class App : RComponent<RProps, RState>() {
+class WebGlCanvasProps(
+        var onMouseUp: (event: MouseEvent) -> Unit,
+        var onMouseDown: (event: MouseEvent) -> Unit,
+        var onMouseMove: (event: MouseEvent) -> Unit,
+        var classes: String = ""
+): RProps
+
+class WebGlCanvas(props: WebGlCanvasProps) : RComponent<WebGlCanvasProps, RState>(props) {
+
+    fun onMouseUp(event: Event) = props.onMouseUp(event as MouseEvent)
+    fun onMouseDown(event: Event) = props.onMouseDown(event as MouseEvent)
+    fun onMouseMove(event: Event) = props.onMouseMove(event as MouseEvent)
+
     override fun RBuilder.render() {
-        div("App-header") {
-            h2 {
-                +"Welcome to React with Kotlin"
-            }
-        }
-        p("App-intro") {
-            +"To get started, edit "
-            code { +"app/App.kt" }
-            +" and save to reload."
-        }
-        p("App-ticker") {
+        canvas(props.classes){
+            attrs.onMouseUpFunction = ::onMouseUp
+            attrs.onMouseDownFunction = ::onMouseDown
+            attrs.onMouseMoveFunction = ::onMouseMove
         }
     }
 }
 
-fun RBuilder.app() = child(App::class) {}
+fun RBuilder.webGlCanvas(props: WebGlCanvasProps) = child(WebGlCanvas::class){
+    attrs.onMouseUp = props.onMouseUp
+    attrs.onMouseDown = props.onMouseDown
+    attrs.onMouseMove = props.onMouseMove
+    attrs.classes = props.classes
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const KotlinWebpackPlugin = require('@jetbrains/kotlin-webpack-plugin');
+
+module.exports = {
+  entry: 'cluster-gl',
+
+  resolve: {
+    modules: ['bin/build', 'node_modules'],
+  },
+
+  output: {
+    path: __dirname + '/bin/bundle',
+    filename: 'cluster-gl.js',
+  },
+
+  plugins: [
+    new KotlinWebpackPlugin({
+      src: __dirname,
+      output: 'bin/test',
+      moduleName: 'cluster-gl',
+      librariesAutoLookup: true,
+      packagesContents: [require('./package.json')],
+    }),
+    new KotlinWebpackPlugin({
+          src: __dirname + '/src',
+          output: 'bin/build',
+          moduleName: 'cluster-gl',
+          sourceMapEmbedSources: 'always',
+          metaInfo: true,
+          sourceMaps: true,
+          librariesAutoLookup: true,
+          packagesContents: [require('./package.json')],
+        }),
+  ],
+};
